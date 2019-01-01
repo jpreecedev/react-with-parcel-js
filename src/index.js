a552a5b6fd2f807ce3388c96735380bd67d3bf52
Jon Preece <jonpreece@hotmail.co.uk>
2019-01-01 15:14:55 +0000

    Added hot module reloading

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app.js";
 
-ReactDOM.render(
-  React.createElement(App, null),
-  document.getElementById("root")
-);
+if (module.hot) {
+  module.hot.accept();
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
